639f3777a7f9cdf16bcd42cfffb6d8b050522dd9
Alex Ermkv <aleksandr.ermkv@yandex.ru>
2022-02-18 12:18:25 +0300

    Add file

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ BOT_TOKEN = "5169616255:AAEtGS9g3If75kGDev6-R-vNxiQtGTPnxsI"
 APP_URL = "https://boxwishesbot.herokuapp.com/" + BOT_TOKEN
 
 
-host = "127.0.0.1"
+host = "0.0.0.0"
 user = "postgres"
 password = "Windsor13"
 port = "5432"
